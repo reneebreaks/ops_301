02c1b47c0bc2bb066764db8e553a4eac03ac8440
Renee Stanley <renee.breaks@gmail.com>
2023-08-27 11:18:07 +0000

    updated hello.py

--- hello/hello.py
+++ hello/hello.py
@@ -1,1 +1,10 @@
-print ("Hello World!")
+
+print("Hi there, what is your name?")
+name = input()
+print("Hello, " + name)
+print("I apologize that I can't just write a one-liner! This is too exciting.")
+print("Okay now " +name + "! Say it along with me on the count of three!")
+print("Here we go!")
+for i in range(1,4)[::1]:
+    print(i) 
+print("Hello World!")
