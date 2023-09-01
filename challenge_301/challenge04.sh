ff9f0079c31d09a2293c25e56a8c1c6672ef3bd2
Renee Stanley <renee.breaks@gmail.com>
2023-09-01 02:38:39 +0000

    edited challenge04

--- challenge_301/challenge04.sh
+++ challenge_301/challenge04.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-#ostype = $OSTYPE
+#Renee Stanley
+#August 31, 2023
 
+#Practicing menus with conditionals
+#Here we have a menu with conditionals within a menu with conditionals using while loops.
+
+#initial prompt and
 while true; do
     clear
     echo "Hello human. I am a simple program, I can only perform a few simple tasks. Please choose one: "
@@ -15,11 +20,15 @@ while true; do
     if [[ $choice == 1 ]]; then
         echo "Hello, World!"
         read -p "Press Enter to continue"
+    
+    #Here we specify the numnber of packets to send for the ping wit -c. I chose three. Otherwise spam.
     elif [[ $choice == 2 ]]; then
         ping -c 3 localhost
         read -p "Press Enter to continue"
+    
+    #The third choice should have choices within it since we don't know what operating system to user is running. So we ask...
     elif [[ $choice == 3 ]]; then
-       
+        
         while true; do
             clear
             echo "What is your operating system?: " ostype
@@ -29,7 +38,7 @@ while true; do
             echo "4. Return to main menu"
             read ostype
 
-       
+            #Then we take the users input and give them the appropriate command to find their ip address given their operating system.
             if [[ "$ostype" == 1 ]]; then
                 ip a
                 read -p "Press Enter to continue"
