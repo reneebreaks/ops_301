059cf971ad1c89d1f6fb6e3d2d338c0735910198
Renee Stanley <renee.breaks@gmail.com>
2023-09-17 22:45:45 +0000

    added challenge02.sh

---
+++ challenge_301/challenge02.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#Renee Stanley
+#Append Date and Time in Bash
+
+#we are going to copy some log files from their home and bring 'em over here with a brand new datetime stamp.
+
+
+
+
+
+#Renee Stanley
+#Append Date and Time in Bash
+
+
+sudo su
+ cp -r /var/log/syslog  $pwd1 >> $current_time
+
+
+
+
+# End
